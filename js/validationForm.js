5697d471789e96d1ad33b5d91204cac2426d4866
mwang840 <maxwang@udel.edu>
2023-09-23 21:45:25 -0400

    email sending worked

--- js/validationForm.js
+++ js/validationForm.js
@@ -1,46 +1,31 @@
-$(document).ready(function() {
-  $("#go").on("click", function (event) {
-    const information = {
-      firstName: document.getElementById("fname").value,
-      lastName: document.getElementById("lname").value,
-      email: document.getElementById("user_email").value,
-      phoneNum: document.getElementById("user_phone").value,
-      message: document.getElementById("user_message").value
-    }
-    const emailValidator = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
-    const serviceID = "service_pdhhje";
-    const templateID = "template_onmrs6j";
-    const publicKey = "LsnVrpJEioYyWQ1ji";
-    emailjs.init(publicKey);
-    //Conditions to add if any of those forms is null or empty
-    if (
-      information.firstName === null ||
-      information.lastName === null ||
-      information.email === null ||
-      information.phoneNum === null ||
-      information.message === null ||
-      information.firstName === "" ||
-      information.lastName === "" ||
-      information.email === "" ||
-      information.phoneNum === "" ||
-      information.message === ""
-    ) {
-      return false;
-    } else if (information.email !== null && emailValidator.test(email) === false) {
-      return false;
-    }
-    else{
-      emailjs.send(serviceID, templateID, information).then(
-        result => {
-          document.getElementById("fname").value = "";
-          document.getElementById("lname").value = "";
-          document.getElementById("user_email").value = "";
-          document.getElementById("user_phone").value = "";
-          document.getElementById("user_message").value = "";
-          console.log(result);
-        }
-      )
-      .catch((err)=> console.log(err));
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    var firstName = document.querySelector("#user_fname").value;
+    var lastName = document.querySelector("#user_lname").value;
+    var email = document.querySelector("#user_email").value;
+    var phoneNum = document.querySelector("#user_phone").value;
+    var message = document.querySelector("#user_message").value;
+    const content = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      phoneNum: phoneNum,
+      message: message,
+    };
+    emailjs.init("LsnVrpJEioYyWQ1ji");
+    emailjs
+      .send("service_20w6kba", "template_onmrs6j", content)
+      .then(function (mes) {
+        console.log("Message Sent");
+        firstName = "";
+        lastName = "";
+        email = "";
+        phoneNum = "";
+        message = "";
+      })
+      .catch(function (eror) {
+        console.log("Email failed to send");
+      });
   });
 });

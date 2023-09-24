e5a209f863632359d44a8a99ab839f58580782e3
mwang840 <maxwang@udel.edu>
2023-09-24 10:33:06 -0400

    add in regex for email and phone and added in not null conditions

--- js/validationForm.js
+++ js/validationForm.js
@@ -13,19 +13,47 @@ document.addEventListener("DOMContentLoaded", function () {
       phoneNum: phoneNum,
       message: message,
     };
-    emailjs.init("LsnVrpJEioYyWQ1ji");
-    emailjs
-      .send("service_20w6kba", "template_onmrs6j", content)
-      .then(function (mes) {
-        console.log("Message Sent");
-        firstName = "";
-        lastName = "";
-        email = "";
-        phoneNum = "";
-        message = "";
-      })
-      .catch(function (eror) {
-        console.log("Email failed to send");
-      });
+    const phoneRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    const emailRegex =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    let validInfo = false;
+    if (!phoneRegex.test(phoneNum)) {
+      console.log("Invalid phone number");
+      return false;
+    } else if (!emailRegex.test(email)) {
+      console.log("Invalid email");
+      return false;
+    } else if (
+      content.firstName === null ||
+      content.lastName === null ||
+      content.message === null ||
+      content.phoneNum === null ||
+      content.email === null ||
+      content.firstName === "" ||
+      content.lastName === "" ||
+      content.message === "" ||
+      content.phoneNum === "" ||
+      content.email === ""
+    ) {
+      console.log("Cant leave forms empty, please fill out the forms");
+      return false;
+    } else {
+      emailjs.init("LsnVrpJEioYyWQ1ji");
+      emailjs
+        .send("service_20w6kba", "template_onmrs6j", content)
+        .then(function (mes) {
+          console.log("Message Sent");
+          firstName = "";
+          lastName = "";
+          email = "";
+          phoneNum = "";
+          message = "";
+        })
+        .catch(function (eror) {
+          console.log("Email failed to send");
+        });
+    }
+    
   });
+
 });

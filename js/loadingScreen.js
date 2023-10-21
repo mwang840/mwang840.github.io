5226c3752ea7bed9ecf850e72e11f849b5c66350
mwang840 <maxwang@udel.edu>
2023-10-20 22:12:57 -0400

    loading page on home implemented

---
+++ js/loadingScreen.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const overlap = document.querySelector(".presenting");
+  const start = document.querySelector("#load");
+  const progressLoad = document.querySelector(".loading-progress");
+
+  document.body.style.overflow = "hidden";
+  let currentProgress = 0;
+  const timeInterval = setInterval(() => {
+    if (currentProgress >= 300) {
+      clearInterval(timeInterval);
+      overlap.style.display = "none";
+      start.style = "block";
+      document.body.style.overflow = "auto";
+    } else {
+      currentProgress += 5;
+      progressLoad.style.width = `${currentProgress}%`;
+    }
+  });
+});
